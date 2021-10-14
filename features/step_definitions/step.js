ab37351fef5fd2e4aeed19818d12f6cde6d08c18
andreayalad <alejandrayala01@gmail.com>
2021-10-14 11:07:00 -0600

    Cleaning Up pt. 2

--- features/step_definitions/step.js
+++ features/step_definitions/step.js
@@ -5,21 +5,18 @@
 
   Before(function () {
     this.network = new Network()
+    this.people = {}
   })
 
-  Given('a person named Lucy', function() {
-    this.lucy = new Person(this.network)
-  })
-
-  Given('a person named Sean', function() {
-    this.sean = new Person(this.network)
+  Given('a person named {word}', function(name) {
+    this.people[name] = new Person(this.network)
   })
 
   When('Sean shouts {string}', function (message) {
-    this.sean.shout(message)
+    this.people['Sean'].shout(message)
     this.messageFromSean = message
   })
 
   Then('Lucy should hear Sean\'s message', function () {
-    assertThat(this.lucy.messagesHeard(), is ([this.messageFromSean]))
+    assertThat(this.people['Lucy'].messagesHeard(), is ([this.messageFromSean]))
   })
